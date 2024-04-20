47b851d2533017d75205cd5751ffd5896a0466e5
levap-21 <igorplatov2002@mail.ru>
2024-04-20 17:38:44 +0300

    task2

---
+++ task2/task2.py
@@ -0,0 +1,45 @@
+import sys
+
+
+def read_circle_file(file_path):
+    with open(file_path, 'r') as file:
+        data = file.readlines()
+        x, y = map(float, data[0].split())
+        radius = float(data[1])
+
+    return x, y, radius
+
+def read_dots_file(file_path):
+    with open(file_path, 'r') as file:
+        points = []
+        for line in file:
+            x, y = map(float, line.split())
+            points.append((x, y))
+
+    return points
+
+def dot_position(x_c, y_c, radius, x_d, y_d):
+    distance_squared = (x_d - x_c) ** 2 + (y_d - y_c) ** 2
+    if distance_squared == radius ** 2:
+        return 0
+    elif distance_squared < radius ** 2:
+        return 1
+    else:
+        return 2
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("write in the format - python" +
+              "\\path_to_file\\task2.py \\path_to_file\\circle.txt \\path_to_file\\dots.txt")
+        sys.exit(1)
+
+    circle_file = sys.argv[1]
+    dots_file = sys.argv[2]
+
+    x_c, y_c, radius = read_circle_file(circle_file)
+    dots = read_dots_file(dots_file)
+
+    for dot in dots:
+        position = dot_position(x_c, y_c, radius, dot[0], dot[1])
+        print(position)
